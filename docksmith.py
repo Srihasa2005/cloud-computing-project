12b8b226a594cc2288d32112109bd701976811a5
Srihasa2005 <srihasa22@gmail.com>
2026-04-15 21:46:45 +0530

    Add environment variable parsing support

--- docksmith.py
+++ docksmith.py
@@ -9,6 +9,18 @@ from builder import build
 from runtime import run_image
 
 
+def parse_env_variables(env_list):
+    env_dict = {}
+
+    if env_list:
+        for item in env_list:
+            if "=" in item:
+                key, value = item.split("=", 1)
+                env_dict[key] = value
+
+    return env_dict
+
+
 def main():
     parser = argparse.ArgumentParser(description="Docksmith CLI")
 
@@ -16,6 +28,7 @@ def main():
     parser.add_argument("tag", nargs="?")
     parser.add_argument("-t", "--tag-name")
     parser.add_argument("--no-cache", action="store_true")
+    parser.add_argument("-e", "--env", action="append", help="Environment variables")
 
     args = parser.parse_args()
 
@@ -35,7 +48,8 @@ def main():
             print("Error: Please provide image tag")
             return
 
-        run_image(image_tag, {})
+        env_vars = parse_env_variables(args.env)
+        run_image(image_tag, env_vars)
 
 
 if __name__ == "__main__":
